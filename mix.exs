c16622dd144a8f1ab7b73499e0eec855b62e1a60
Ivan Youroff <ivan.youroff@gmail.com>
2020-09-15 11:01:52 +0200

    elixir version bump due to is_struct guard use

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StateMachine.MixProject do
   def project, do: [
     app: :state_machine,
     version: "0.1.3",
-    elixir: "~> 1.7",
+    elixir: "~> 1.10",
     start_permanent: Mix.env() == :prod,
     elixirc_paths: elixirc_paths(Mix.env),
     description: description(),
